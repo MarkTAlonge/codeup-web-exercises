f209aeb896f284a15d07997c2377c2cde94443d9
Mark Alonge <Mark.T.Alonge@gmail.com>
2020-05-18 10:17:49 -0500

    add exercises

--- js/jquery-faq.js
+++ js/jquery-faq.js
@@ -1,8 +1,26 @@
 "use strict";
 $(document).ready(function() {
-$('dd').addClass('invisible');
-});
+    $('dd').addClass('invisible');
 
-$('dt').click(function(){
-    $(this).toggleClass('invisible')
 });
+    $('dt').click(function () {
+        $(this).toggleClass('invisible');
+    });
+
+    $('#highlight').click(function () {
+        $('ul').each(function () {
+            $(this).children().last().css("background-color", 'yellow');
+        });
+    });
+
+        $('h3').click(function () {
+            $(this).next().children().css('font-weight', 'bold')
+        });
+
+        $('li').click(function () {
+            $(this).parent().children().first().css('color', 'blue');
+
+        });
+
+
+
